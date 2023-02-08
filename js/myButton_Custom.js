ef9749b70defd3732cf186db7cda9513237efba0
Shirado <office-shirado@users.noreply.github.com>
2023-02-08 11:41:08 +0900

    Add files via upload

---
+++ js/myButton_Custom.js
@@ -0,0 +1,18 @@
+﻿//myButton（ボタンカスタム）
+
+//位置取得（現在地）
+var button_Locate = L.myButton(
+	'fa-map-marker',					//アイコン
+       	//ファンクション
+	function (){
+		map.locate({
+			watch: false,
+			timeout: 10000,
+			maximumAge: 0,
+			enableHighAccuracy: true,
+			setView: true
+		});
+//		getLocate();
+	},
+	'現在地'					//ホバーテキスト
+);
