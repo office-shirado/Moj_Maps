1ce69c3a8a6a09a1fe6ce4f9be5d34efc0c02a77
Shirado <office-shirado@users.noreply.github.com>
2023-02-11 14:47:52 +0900

    Add files via upload

--- MapLibre/js/main.js
+++ MapLibre/js/main.js
@@ -1,80 +1,65 @@
 var map = new maplibregl.Map({
     container: 'map',
     style: {
-		version: 8,
-		sources: {
+        center: [140.8835,37.0509], // 中心座標
+        zoom: 16, // ズームレベル
+        version: 8,
+	sources: {
             rtile: {
                 type: 'raster',
                 tiles: [
                     'https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg',
                 ],
                 tileSize: 256,
-                attribution:
-                    "<a href='https://www.gsi.go.jp/' target='_blank'>地理院タイル</a>",
+                attribution:"<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>",
             },
-			plateau: {
-			type: 'vector',
-			tiles: ['https://x.optgeo.org/a/{z}/{x}/{y}.mvt'],
-			minzoom: 10,
-			maxzoom: 23,
-			attribution:
-				"データの出典：<a href='https://github.com/indigo-lab/plateau-tokyo23ku-building-mvt-2020'>plateau-tokyo23ku-building-mvt-2020 by indigo-lab</a> (<a href='https://www.mlit.go.jp/plateau/'>国土交通省 Project PLATEAU</a> のデータを加工して作成)",
+            plateau: {
+                type: 'vector',
+                tiles: ['https://x.optgeo.org/a/{z}/{x}/{y}.mvt'],
+                minzoom: 14,
+                maxzoom: 16,
+                attribution:"<a href='https://www.moj.go.jp/MINJI/minji05_00494.html' target='_blank'>法務省地図</a>",
 			},
 		},
 
 
+            layers: [
+                {
+                  id: 'seamlessphoto',
+                  type: 'raster',
+                  source: 'rtile',
+                  minzoom: 0,
+                  maxzoom: 23,
+                },
 
-		layers: [
-            {
-                id: 'raster-tiles',
-                type: 'raster',
-                source: 'rtile',
-                minzoom: 0,
-                maxzoom: 23,
-            },
-
-        {
-            id: 'fude',
-            type: 'fill',
-            source: 'plateau',
-            'source-layer': 'fude',
-            minzoom: 14,
-            maxzoom: 23,
-	      'paint': {
-    	      "fill-color": "#ffff00",
-		  "fill-opacity": 0.2
-			},
-		},
-
-        {
-            id: 'fude2',
-            type: 'line',
-            source: 'plateau',
-            'source-layer': 'fude',
-            minzoom: 14,
-            maxzoom: 23,
-	      'paint': {
-    	      "line-color": "#ff0000",
-           "line-width": 1,
-			},
-		}
+                {
+                  id: 'fude-fill',
+                  type: 'fill',
+                  source: 'plateau',
+                  'source-layer': 'fude',
+                  minzoom: 14,
+                  maxzoom: 23,
+                  'paint': {
+                    "fill-color": "#ffff00",
+                    "fill-opacity": 0.2
+                  },
+                },
 
 
-		]
-
-
-
-
+                {
+                  id: 'fude-line',
+                  type: 'line',
+                  source: 'plateau',
+                  'source-layer': 'fude',
+                  minzoom: 14,
+                  maxzoom: 23,
+                  'paint': {
+                    "line-color": "#ff0000",
+                  },
+                },
+            ]
     },
-    center: [140.8835,37.0509], // 中心座標
-    zoom: 16, // ズームレベル
 });
-
-
-
-
-
-
 
 
 
